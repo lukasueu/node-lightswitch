78017880a83678612ee32ac97d575975c9528a8c
lukasueu <lukasueu@gmail.com>
2021-05-06 16:19:08 +0700

    update

--- public/js/lightswitch.js
+++ public/js/lightswitch.js
@@ -5,7 +5,9 @@ ws.onopen = function (event) {
   $('img').on('click', sendState);
 };
 
-var on = "img/on.jpg";
+var on_green = "img/on_green.jpg";
+var on_yellow = "img/on_yellow.jpg";
+var on_red = "img/on_red.jpg";
 var off = "img/off.jpg";
 var obj;
 
@@ -15,29 +17,29 @@ ws.onmessage = function (event) {
   $('#userCount').html(obj.users);
 
   if (obj.switch1 == 0) {
-    $('#switch1').attr('src', on);
-  }
-  else {
     $('#switch1').attr('src', off);
   }
-  if (obj.switch2 == 0) {
-    $('#switch2').attr('src', on);
-  }
   else {
+    $('#switch1').attr('src', on_green);
+  }
+  if (obj.switch2 == 0) {
     $('#switch2').attr('src', off);
   }
+  else {
+    $('#switch2').attr('src', on_yellow);
+  }
   if (obj.switch3 == 0) {
-    $('#switch3').attr('src', on);
+    $('#switch3').attr('src', off);
   }
   else {
-    $('#switch3').attr('src', off);
+    $('#switch3').attr('src', on_red);
   }
 }
 
 //sends updates state of switches to server
 function sendState() {
   if ($(this).attr('id') == 'switch1') {
-    if ($('#switch1').attr('src') == on) {
+    if ($('#switch1').attr('src') == off) {
       obj.switch1 = 1;
     }
     else {
@@ -45,7 +47,7 @@ function sendState() {
     }
   }
   else if ($(this).attr('id') == 'switch2') {
-    if ($('#switch2').attr('src') == on) {
+    if ($('#switch2').attr('src') == off) {
       obj.switch2 = 1;
     }
     else {
@@ -53,7 +55,7 @@ function sendState() {
     }
   }
   else if ($(this).attr('id') == 'switch3') {
-    if ($('#switch3').attr('src') == on) {
+    if ($('#switch3').attr('src') == off) {
       obj.switch3 = 1;
     }
     else {

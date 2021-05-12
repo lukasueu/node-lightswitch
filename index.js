8eeccc3e373b47c038ef4cb5e4842d6a74cffebd
lukasueu <lukasueu@gmail.com>
2021-05-12 11:53:06 +0700

    update

--- index.js
+++ index.js
@@ -15,14 +15,10 @@ var wss = new WebSocketServer({server: server});
 console.log("websocket server created");
 
 var users = 0;
-// 0 is on; 1 is off
-var switch1 = 0;
-var switch2 = 0;
-var switch3 = 0;
 
 wss.on("connection", function(ws) {
   users++;
-  var obj = {users: users, switch1: switch1, switch2: switch2, switch3: switch3};
+  var obj = {users: users};
   wss.broadcast(JSON.stringify(obj));
 
   console.log("websocket connection open");
@@ -33,17 +29,7 @@ wss.on("connection", function(ws) {
     wss.broadcast(JSON.stringify(obj));
 
   })
-
-  ws.on("message", function (message) {
-	wss.broadcast(message);
-	var messageobj = JSON.parse(message);
-	switch1 = messageobj.switch1;
-	switch2 = messageobj.switch2;
-	switch3 = messageobj.switch3;
-  })
-
 })
-
 
 wss.broadcast = function broadcast(data) {
 	wss.clients.forEach(function each(client) {
--- public/js/lightswitch.js
+++ public/js/lightswitch.js
@@ -1,39 +1,12 @@
 var host = location.origin.replace(/^http/, 'ws');
 var ws = new WebSocket(host);
 
-ws.onopen = function (event) {
-  $('img').on('click', sendState);
-};
-
-var on_green = "img/on_green.jpg";
-var on_yellow = "img/on_yellow.jpg";
-var on_red = "img/on_red.jpg";
-var off = "img/off.jpg";
 var obj;
 
 //gets state of switches from server
 ws.onmessage = function (event) {
   obj = JSON.parse(event.data);
   $('#userCount').html(obj.users);
-
-  if (obj.switch1 == 0) {
-    $('#switch1').attr('src', off);
-  }
-  else {
-    $('#switch1').attr('src', on_green);
-  }
-  if (obj.switch2 == 0) {
-    $('#switch2').attr('src', off);
-  }
-  else {
-    $('#switch2').attr('src', on_yellow);
-  }
-  if (obj.switch3 == 0) {
-    $('#switch3').attr('src', off);
-  }
-  else {
-    $('#switch3').attr('src', on_red);
-  }
 
   if (obj.temp) {
     $('#temp').html(obj.temp + "&#8451;");
@@ -53,33 +26,4 @@ ws.onmessage = function (event) {
   else {
     $('#humidity').html("");
   }
-}
-
-//sends updates state of switches to server
-function sendState() {
-  if ($(this).attr('id') == 'switch1') {
-    if ($('#switch1').attr('src') == off) {
-      obj.switch1 = 1;
-    }
-    else {
-      obj.switch1 = 0;
-    }
-  }
-  else if ($(this).attr('id') == 'switch2') {
-    if ($('#switch2').attr('src') == off) {
-      obj.switch2 = 1;
-    }
-    else {
-      obj.switch2 = 0;
-    }
-  }
-  else if ($(this).attr('id') == 'switch3') {
-    if ($('#switch3').attr('src') == off) {
-      obj.switch3 = 1;
-    }
-    else {
-      obj.switch3 = 0;
-    }
-  }
-    ws.send(JSON.stringify(obj));
 }
